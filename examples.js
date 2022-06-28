6b3e13d592145b7fb7e1089cf577ffbcc143e252
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-06-28 18:34:42 -0400

    update linkedlist alternative method

--- examples.js
+++ examples.js
@@ -1,64 +1,115 @@
+// class Linkedlist {
+//   constructor() {
+//     (this.head = null), (this.tail = null);
+//   }
+//   append(value) {
+//     const newNode = { value: value, next: null };
+//     if (this.tail) {
+//       this.tail.next = newNode;
+//     }
+//     this.tail = newNode;
+//     if (!this.head) {
+//       this.head = newNode;
+//     }
+//   }
+
+//   delete(value) {
+//     if(!this.head) {
+//       return;
+//     }
+//     while (this.head && this.head.value === value) {
+//       this.head = this.head.next;
+//     }
+//     let curNode = this.head;
+//     while(curNode.next) {
+//       if(curNode.next.value === value) {
+//         curNode.next = curNode.next.next;
+//       } else {
+//         curNode = curNode.next;
+//       }
+//     }
+//     if (this.tail.value === value) {
+//       this.tail = curNode;
+//     }
+//   }
+
+//   preppend(value) {
+//     const newNode = { value: value, next: this.head };
+//     this.head = newNode;
+//     if(!this.tail) {
+//       this.tail = newNode;
+//     }
+//   }
+
+//   toArray() {
+//     const element = [];
+//     let curNode = this.head;
+//     while (curNode) {
+//       element.push(curNode);
+//       curNode = curNode.next;
+//     }
+//     return element;
+//   }
+// }
+
+// const Linkedlist1 = new Linkedlist();
+// Linkedlist1.append("Ikenna");
+// Linkedlist1.append("John");
+// Linkedlist1.append("Cynthia");
+// Linkedlist1.append("Ikenna");
+// Linkedlist1.append("David");
+// Linkedlist1.append("Ikenna");
+// console.log(Linkedlist1.toArray());
+// Linkedlist1.delete("Ikenna");
+// console.log(Linkedlist1.toArray());
+
+// We have to implement append, prepend, delete, and toArray
 class Linkedlist {
   constructor() {
-    (this.head = null), (this.tail = null);
+    this.head = null;
+    this.tail = null;
   }
-  append(value) {
-    const newNode = { value: value, next: null };
+
+  append(input) {
+    const newNode = { value: input, next: null };
     if (this.tail) {
       this.tail.next = newNode;
     }
+
     this.tail = newNode;
     if (!this.head) {
       this.head = newNode;
     }
   }
 
-  delete(value) {
-    if(!this.head) {
-      return;
-    }
-    while (this.head && this.head.value === value) {
-      this.head = this.head.next;
-    }
-    let curNode = this.head;
-    while(curNode.next) {
-      if(curNode.next.value === value) {
-        curNode.next = curNode.next.next;
-      } else {
-        curNode = curNode.next;
-      }
-    }
-    if (this.tail.value === value) {
-      this.tail = curNode;
-    }
-  }
-
-  preppend(value) {
-    const newNode = { value: value, next: this.head };
-    this.head = newNode;
-    if(!this.tail) {
-      this.tail = newNode;
-    }
-  }
-
   toArray() {
-    const element = [];
+    const nodes = [];
     let curNode = this.head;
     while (curNode) {
-      element.push(curNode);
+      nodes.push(curNode);
       curNode = curNode.next;
     }
-    return element;
+    return nodes;
+  }
+  preppend(input) {
+    const newNode = { value: input, next: this.head };
+    if (!this.head) {
+      this.head = newNode;
+    }
+    this.head = newNode;
+    if (!this.tail) {
+      this.tail = newNode;
+    }
   }
 }
 
 const Linkedlist1 = new Linkedlist();
+
 Linkedlist1.append("Ikenna");
-Linkedlist1.append("John");
+Linkedlist1.append("Ike");
+Linkedlist1.preppend("Ike");
 Linkedlist1.append("Cynthia");
-Linkedlist1.append("Ikenna");
-Linkedlist1.append("David");
-Linkedlist1.append("Ikenna");
-console.log(Linkedlist1.toArray()); 
-Linkedlist1.delete("Ikenna");
-console.log(Linkedlist1.toArray()); 
+Linkedlist1.append("Ike");
+// Linkedlist1.delete("Ike");
+
+console.log(Linkedlist1.toArray());
