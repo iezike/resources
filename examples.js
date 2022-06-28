257d0a6daeef01888655bfffec93a95debbe07e2
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-06-27 21:01:00 -0400

    update code with examples

---
+++ examples.js
@@ -0,0 +1,9 @@
+const names = ["John", "John", "Julie"] 
+const johnIndex = names.find(name => name === "Julie")
+// console.log(johnIndex)
+
+
+const numbers = [1, 3, 6, 8];
+
+const evenNumbers = numbers.find(number => number % 2 === 0)
+console.log(evenNumbers)
