917939d0b4be998a34d43d724beaef6fb6a3618d
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-06-28 13:17:13 -0400

    update linked list class

--- examples.js
+++ examples.js
@@ -1,9 +1,64 @@
-const names = ["John", "John", "Julie"] 
-const johnIndex = names.find(name => name === "Julie")
-// console.log(johnIndex)
+class Linkedlist {
+  constructor() {
+    (this.head = null), (this.tail = null);
+  }
+  append(value) {
+    const newNode = { value: value, next: null };
+    if (this.tail) {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
+    if (!this.head) {
+      this.head = newNode;
+    }
+  }
 
+  delete(value) {
+    if(!this.head) {
+      return;
+    }
+    while (this.head && this.head.value === value) {
+      this.head = this.head.next;
+    }
+    let curNode = this.head;
+    while(curNode.next) {
+      if(curNode.next.value === value) {
+        curNode.next = curNode.next.next;
+      } else {
+        curNode = curNode.next;
+      }
+    }
+    if (this.tail.value === value) {
+      this.tail = curNode;
+    }
+  }
 
-const numbers = [1, 3, 6, 8];
+  preppend(value) {
+    const newNode = { value: value, next: this.head };
+    this.head = newNode;
+    if(!this.tail) {
+      this.tail = newNode;
+    }
+  }
 
-const evenNumbers = numbers.find(number => number % 2 === 0)
-console.log(evenNumbers)
+  toArray() {
+    const element = [];
+    let curNode = this.head;
+    while (curNode) {
+      element.push(curNode);
+      curNode = curNode.next;
+    }
+    return element;
+  }
+}
+
+const Linkedlist1 = new Linkedlist();
+Linkedlist1.append("Ikenna");
+Linkedlist1.append("John");
+Linkedlist1.append("Cynthia");
+Linkedlist1.append("Ikenna");
+Linkedlist1.append("David");
+Linkedlist1.append("Ikenna");
+console.log(Linkedlist1.toArray()); 
+Linkedlist1.delete("Ikenna");
+console.log(Linkedlist1.toArray()); 
