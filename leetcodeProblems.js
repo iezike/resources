d895bc8c2a30c1496bb4d8207d8a352dea982684
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-06-29 23:19:18 -0400

    implement LC 27. Remove Element

--- leetcodeProblems.js
+++ leetcodeProblems.js
@@ -7,20 +7,65 @@ const twoSum = function (nums, target) {
     }
     storage[target - num] = i;
   }
-  return [-1, -1];
 };
 // console.log(twoSum([2, 7, 11, 15], 17))
 // ---------------------------------------------------------
 
+// 27. Remove Element
+const removeElement = function (nums, val) {
+  if (!nums) return 0;
+  const len = nums.length;
+  for (let i = 0; i < len; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--
+    }
+  }
+  return nums.length;
+};
+
+// console.log(removeElement([0,1,2,2,3,0,4,2],2))
+
+// ---------------------------------------------------------
+
+// 47. Permutations II
+var dfs = function (res, arr, nums) {
+  var len = nums.length;
+  var tmp1 = null;
+  var tmp2 = null;
+
+  if (!len) return res.push(arr);
+
+  for (var i = 0; i < len; i++) {
+    if (nums[i] === nums[i - 1]) continue;
+
+    tmp1 = Array.from(arr);
+    tmp1.push(nums[i]);
+
+    tmp2 = Array.from(nums);
+    tmp2.splice(i, 1);
+
+    dfs(res, tmp1, tmp2);
+  }
+};
+
+var permuteUnique = function (nums) {
+  var res = [];
+  nums.sort((a, b) => a - b);
+  dfs(res, [], nums);
+  return res;
+};
+// console.log(permuteUnique([1,1,2]))
+// ---------------------------------------------------------
+
 // 48. Rotate Image
 var swap = function (matrix, x1, y1, x2, y2) {
-
   var tmp = matrix[x1][y1];
   matrix[x1][y1] = matrix[x2][y2];
   matrix[x2][y2] = tmp;
 };
 
-var rotate = function(matrix) {
+var rotate = function (matrix) {
   var n = matrix.length;
   var n2 = Math.floor(n / 2);
   // 1 2 3     7 8 9
@@ -42,7 +87,6 @@ var rotate = function(matrix) {
   }
   return matrix;
 };
-
 
 // console.log(rotate([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]))
 
