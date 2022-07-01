a69587d9c4961d83b7333db65db649bbf0d705b3
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-06-30 23:57:48 -0400

    implement LC Permutations

--- leetcodeProblems.js
+++ leetcodeProblems.js
@@ -127,18 +127,15 @@ var dfs = function (res, arr, nums) {
   var tmp2 = null;
 
   if (!len) return res.push(arr);
-
   for (var i = 0; i < len; i++) {
     tmp1 = Array.from(arr);
     tmp1.push(nums[i]);
-
+    console.log(tmp1)
     tmp2 = Array.from(nums);
     tmp2.splice(i, 1);
-
     dfs(res, tmp1, tmp2);
   }
 };
-
 // ------------------------------------------------------
 
 // 47. Permutations II
