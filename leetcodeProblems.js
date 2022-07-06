13eb6175ae0f64bad1b72f4c7eb3fca7fb907c34
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-05 23:53:42 -0400

    refactor solution

--- leetcodeProblems.js
+++ leetcodeProblems.js
@@ -668,7 +668,36 @@ const maxSubArray = function (nums) {
 
 // console.log(maxSubArray([7,-1,-3,-4,-1,2,1,-5,4]))
 
-
+// 54. Spiral Matrix
+const spiralOrder = function (matrix) {
+  let top = 0;
+  let left = 0;
+  let right = matrix[0].length - 1;
+  let bottom = matrix.length - 1;
+  let size = matrix.length * matrix[0].length;
+  let result = [];
+  while (result.length < size) {
+    for (let i = left; i <= right; i++) {
+      result.push(matrix[top][i]);
+    }
+    top++;
+    for (let i = top; i <= bottom; i++) {
+      result.push(matrix[i][right]);
+    }
+    right--;
+    for (let i = right; i >= left; i--) {
+      result.push(matrix[bottom][i]);
+    }
+    bottom--;
+    for (let i = bottom; i >= top; i--) {
+      result.push(matrix[i][left]);
+    }
+    left++;
+  }
+  return result;
+};
+// T: O(n * m), S: O(n *m);
+// console.log(spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
 
 // 73. Set Matrix Zeroes
 let setZeros = function (row, col, matrix) {
@@ -696,13 +725,13 @@ let setZeroes = function (matrix) {
   return matrix;
 };
 
-console.log(
-  setZeroes([
-    [1, 1, 1],
-    [1, 0, 1],
-    [1, 1, 1],
-  ])
-);
+// console.log(
+//   setZeroes([
+//     [1, 1, 1],
+//     [1, 0, 1],
+//     [1, 1, 1],
+//   ])
+// );
 
 // 102. Binary Tree Level Order Traversal
 class TreeNode {
