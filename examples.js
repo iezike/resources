207b7005363d5296fd432e9e601baa2933011633
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-10 21:48:23 -0400

    implement sum Zero

--- examples.js
+++ examples.js
@@ -357,3 +357,21 @@ function isAnagrams(arr1, arr2) {
   return true;
 }
 // console.log(isAnagrams("azz", "zaz"));
+
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    const sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  return null;
+}
+
+// console.log(sumZero([-4, -1, 0, 1, 2, 4]));
