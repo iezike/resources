65636cb48a7f1595171cf881a9ea1c2e0a1eb685
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-11 18:00:00 -0400

    implement indexOf function with O(n)

--- examples.js
+++ examples.js
@@ -433,4 +433,15 @@ function factorial(n) {
   return n + factorial(n-1);
 }
 
-console.log(factorial(3))
+// console.log(factorial(3))
+
+function indexOf(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(indexOf([2,8,9,-1,4], 10))
