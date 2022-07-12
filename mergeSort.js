8103007365143484c88e94ad91ac35e986652cb9
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-12 19:38:18 -0400

    merge two sorted arrays

---
+++ mergeSort.js
@@ -0,0 +1,25 @@
+function mergeSort(arr1, arr2) {
+  const result = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return result;
+}
+
+console.log(mergeSort([1, 14], [2, 4, 8, 10]));
