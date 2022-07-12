af28d973e37c2be648e8343c8aaf4762b09975a8
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-11 23:09:07 -0400

    refactor

--- binarySearch.js
+++ binarySearch.js
@@ -2,16 +2,16 @@ function binarySearch(arr, val) {
   let left = 0;
   let right = arr.length - 1;
   while (left <= right) {
-    let midIndex = Math.floor((left + right) / 2)
+    let midIndex = Math.floor((left + right) / 2);
     const mid = arr[midIndex];
     if (mid === val) {
       return midIndex;
     } else if (mid > val) {
-      right = midIndex+1;
+      right = midIndex - 1;
     } else {
       left = midIndex + 1;
     }
   }
   return -1;
 }
-console.log(indexOfBin([-1,2, 8, 9, 11], 4));
+console.log(binarySearch([-1, 2, 8, 9, 11], 2));
