e779586ffcefb0bf4160c913b64ae411e5b9deb9
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-12 07:28:20 -0400

    implement naive sort

---
+++ naiveSort.js
@@ -0,0 +1,15 @@
+function naiveSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(naiveSort([1, -2, 3, 4, 9, 8]));
