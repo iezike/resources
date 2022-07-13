1f0655b3c0515bf35f0f2698fda4b029e5b59c94
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-13 19:39:52 -0400

    implement enqueue method

---
+++ dataStructure/queue.js
@@ -0,0 +1,30 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++
+    return this.size;
+  }
+}
+
+const queue = new Queue();
+console.log(queue.enqueue(1));
+
