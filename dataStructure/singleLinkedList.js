26b76e29872ae8be35498cecee0a3e85ddcc49e8
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-13 09:05:54 -0400

    clean code

--- dataStructure/singleLinkedList.js
+++ dataStructure/singleLinkedList.js
@@ -1,11 +1,11 @@
 class Node {
   constructor(val) {
-    this.head = val;
-    this.tail = null;
+    this.val = val;
+    this.next = null;
   }
 }
 class singleLinkedList {
-  constructor() {
+  constructor(val) {
     this.head = null;
     this.tail = null;
     this.length = 0;
@@ -18,8 +18,16 @@ class singleLinkedList {
     } else {
       this.tail.next = newNode;
       this.tail = newNode;
+      // this.next = null
     }
     this.length++;
     return this;
   }
 }
+
+const newlinkedlist = new singleLinkedList();
+newlinkedlist.push(2)
+newlinkedlist.push(4)
+newlinkedlist.push(8)
+newlinkedlist.push(10)
+console.log(newlinkedlist)
