dc4796efdecb2fbdf98a7e014b4a4c433313e186
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-13 08:47:06 -0400

    implement push method

--- dataStructure/singleLinkedList.js
+++ dataStructure/singleLinkedList.js
@@ -1,7 +1,25 @@
+class Node {
+  constructor(val) {
+    this.head = val;
+    this.tail = null;
+  }
+}
 class singleLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
   }
 }
