a26bf48cd741d33d3f92b291ffc8cd152c364ec1
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-14 23:05:46 -0400

    implement breadth first search

---
+++ dataStructure/BFS.js
@@ -0,0 +1,77 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        } else {
+          return undefined;
+        }
+      }
+    }
+  }
+  find(value) {
+    if (!this.root) return undefined;
+    let current = this.root;
+    while (current) {
+      if (current.value === value) {
+        return true;
+      } else if (value < current.value) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return undefined;
+  }
+  BFS() {
+    const data = [],
+      queue = [];
+    let node = this.root;
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+}
+
+const tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.insert(6);
+tree.insert(8);
+tree.insert(3);
+tree.insert(15);
+tree.insert(20);
+console.log(tree.BFS())
