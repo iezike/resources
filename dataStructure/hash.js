84af5378f8d01d9207fde278a638016082bfb794
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-15 22:32:10 -0400

    implement hash function

---
+++ dataStructure/hash.js
@@ -0,0 +1,12 @@
+function hash(key, arrayLen) {
+  let total = 0;
+  for (let char of key) {
+    let value = char.charCodeAt(0) - 96;
+    total = (total + value) % arrayLen;
+  }
+  return total;
+}
+
+
+console.log(hash("pink", 15))
+console.log(hash("blue", 15))
