b08e7f99d363dbea15ca77354bc4d7b0076e83d6
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-15 23:20:09 -0400

    alternative hash function

--- dataStructure/hash.js
+++ dataStructure/hash.js
@@ -1,12 +1,22 @@
+// function hash(key, arrayLen) {
+//   let total = 0;
+//   for (let char of key) {
+//     let value = char.charCodeAt(0) - 96;
+//     total = (total + value) % arrayLen;
+//   }
+//   return total;
+// }
+
 function hash(key, arrayLen) {
   let total = 0;
-  for (let char of key) {
+  const prime = 31;
+  for (let i = 0; i < Math.max(key.length, 100); i++) {
+    let char = key[i];
     let value = char.charCodeAt(0) - 96;
-    total = (total + value) % arrayLen;
+    total = (total * prime + value) % arrayLen;
   }
   return total;
 }
-
 
 console.log(hash("pink", 15))
 console.log(hash("blue", 15))
