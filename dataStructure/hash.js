64d280e940ffe89b29514e986137ad9c0c53b83a
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-15 23:33:57 -0400

    define hash class

--- dataStructure/hash.js
+++ dataStructure/hash.js
@@ -6,17 +6,23 @@
 //   }
 //   return total;
 // }
-
-function hash(key, arrayLen) {
-  let total = 0;
-  const prime = 31;
-  for (let i = 0; i < Math.max(key.length, 100); i++) {
-    let char = key[i];
-    let value = char.charCodeAt(0) - 96;
-    total = (total * prime + value) % arrayLen;
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
   }
-  return total;
+  
+  hash(key, arrayLen) {
+    let total = 0;
+    const prime = 31;
+    for (let i = 0; i < Math.max(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * prime + value) % arrayLen;
+    }
+    return total;
+  }
 }
 
-console.log(hash("pink", 15))
-console.log(hash("blue", 15))
+
+console.log(hash("pink", 15));
+console.log(hash("blue", 15));
