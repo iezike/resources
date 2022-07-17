3500a230b2c90b70b573089851c8e4c43a1b67cb
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-17 18:13:57 -0400

    implement a method to remove an edge in a graph

--- dataStructure/graphs.js
+++ dataStructure/graphs.js
@@ -5,10 +5,21 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+
   addEdge(vertex1, vertex2) {
     if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
       this.adjacencyList[vertex1].push(vertex2);
       this.adjacencyList[vertex2].push(vertex1);
+    }
+  }
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (v) => v !== vertex2
+      );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (v) => v !== vertex1
+      );
     }
   }
 }
@@ -17,4 +28,6 @@ const g = new Graph();
 g.addVertex("Tokyo");
 g.addVertex("Wohan");
 g.addEdge("Tokyo", "Wohan");
+console.log(g.adjacencyList);
+g.removeEdge("Tokyo", "Wohan");
 console.log(g.adjacencyList);
