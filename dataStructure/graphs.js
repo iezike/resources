17abd4b7fc1fa61c90ebab099623718f046264a9
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-17 18:01:32 -0400

    implement a method to add an edge to a graph

--- dataStructure/graphs.js
+++ dataStructure/graphs.js
@@ -5,9 +5,16 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+    }
+  }
 }
 
-const g = new Graph()
+const g = new Graph();
 g.addVertex("Tokyo");
-g.adjacencyList.Tokyo.push("Something")
-console.log(g.adjacencyList)
+g.addVertex("Wohan");
+g.addEdge("Tokyo", "Wohan");
+console.log(g.adjacencyList);
