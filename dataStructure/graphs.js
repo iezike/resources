2fa1e64b885f7a9ece30ff900dd936156349fc2b
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-18 10:56:52 -0400

    create a graph with six vertices and seven edges

--- dataStructure/graphs.js
+++ dataStructure/graphs.js
@@ -33,10 +33,19 @@ class Graph {
 }
 
 const g = new Graph();
-g.addVertex("Tokyo");
-g.addVertex("Wohan");
-g.addEdge("Tokyo", "Wohan");
-console.log(g.adjacencyList);
-g.removeEdge("Tokyo", "Wohan");
-g.removeVertex("Tokyo");
-console.log(g.adjacencyList);
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+
+
