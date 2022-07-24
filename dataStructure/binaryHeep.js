32a752f46055b48c38b8a91e70f410c994c6c4b9
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-23 23:55:29 -0400

    implement insert method for a binary heep

--- dataStructure/binaryHeep.js
+++ dataStructure/binaryHeep.js
@@ -1,5 +1,28 @@
 class MaxBinaryHeep {
   constructor() {
-    this.values = [];
+    this.values = [41, 39, 33, 18, 27, 12];
+  }
+
+  insert(elem) {
+    this.values.push(elem);
+    this.pubbleUp();
+    console.log(this.values[this.values.length - 1]);
+  }
+
+  pubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      const parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
   }
 }
+
+let heep = new MaxBinaryHeep();
+heep.insert(45);
+console.log(heep.values);
