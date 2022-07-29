ff2ac35b63fde4e392b32a06b54807f51cbddb9e
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-29 12:46:12 -0400

    implement naiveSort in both ascending or descesing order

--- naiveSort.js
+++ naiveSort.js
@@ -1,12 +1,11 @@
-function naiveSort(arr) {
-  let noSwap = false;
+// Sort in Increasing Order
+function naiveSortASC(arr) {
   const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
   };
-  for (let i = arr.length; i > 0; i--) {
-    noSwap = true;
-    for (let j = 0; j < i - 1; j++) {
-      console.log(arr, arr[j], arr[j +1])
+  for (let i = arr.length - 1; i > 0; i--) {
+    let noSwap = true;
+    for (let j = 0; j < i; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
         noSwap = false;
@@ -17,4 +16,23 @@ function naiveSort(arr) {
   return arr;
 }
 
-console.log(naiveSort([1, -2, 3, 8, 9]));
+const swapIndex = (arr, idx1, idx2) => {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+};
+
+// Sort in Decendening Order
+function naiveSortDESC(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let swap = false;
+    for (let j = arr.length - 1; j > i; j--) {
+      if (arr[j] > arr[j - 1]) {
+        swapIndex(arr, j, j - 1);
+        swap = true;
+      }
+    }
+    if (!swap) break;
+  }
+  return arr;
+}
+
+console.log(naiveSortASC([9, 7, 3, 4, -2]));
--- quickSort.js
+++ quickSort.js
@@ -2,12 +2,18 @@ function pivot(arr, start = 0, end = arr.length + 1) {
   const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
   };
+  // console.log(swap(arr, 1, 3))
   let pivot = arr[start];
   let swapIndex = 0;
   for (let i = 1; i < arr.length; i++) {
     if (pivot > arr[i]) {
-      swapIndex++;
-      swap(arr, swapIndex, i)
+      swap(arr, swapIndex, i);
+      swapIndex = i;
+      pivot = arr[swapIndex]
     }
+    pivot = arr[i]
   }
+  return arr
 }
+
+console.log(pivot([1,3,5,4]))
