3cbf8acb1077fe61dfcaa7e0f01f040d242c5143
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-07-29 22:31:29 -0400

    implement length of longest norepeat substring of an input string

--- leetCode/lengthOfLongestString.js
+++ leetCode/lengthOfLongestString.js
@@ -1,3 +1,16 @@
 function lenghtOfLongestStr(s) {
-  
+  let seenChars = {};
+  let maxLen = 0;
+  let start = 0;
+  for (let end = 0; end < s.length; end++) {
+    let lastChar = s[end];
+    if (lastChar in seenChars) {
+      start = Math.max(start, seenChars[lastChar] + 1);
+    }
+    seenChars[lastChar] = end;
+    maxLen = Math.max(maxLen, end - start + 1);
+  }
+  return maxLen;
 }
+
+console.log(lenghtOfLongestStr("abcdabcbbjklmo"));
