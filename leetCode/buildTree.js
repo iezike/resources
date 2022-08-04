a8e6b591ca742a63260be6d6ea3230d5c0b68f68
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-08-04 02:44:18 -0400

    build a binary tree from given preorder and inorder traversal

---
+++ leetCode/buildTree.js
@@ -0,0 +1,28 @@
+// Definition for a binary tree node.
+class TreeNode {
+  constructor(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+const buildTree = function (preorder, inorder) {
+  p = i = 0;
+  build = function (stop) {
+    if (inorder[i] != stop) {
+      var root = new TreeNode(preorder[p++]);
+      root.left = build(root.val);
+      i++;
+      root.right = build(stop);
+      return root;
+    }
+    return null;
+  };
+  return build();
+};
+
+const preorder = [3, 9, 20, 15, 7];
+const inorder = [9, 3, 15, 20, 7];
+
+console.log(buildTree(preorder, inorder));
