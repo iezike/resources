97fc8854901c0274c95861fe3a57fed3bdb79650
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-08-04 23:20:24 -0400

    refactor

--- leetCode/longestSubString.js
+++ leetCode/longestSubString.js
@@ -17,4 +17,21 @@ function longestSubString(str) {
   return maxCount;
 }
 
+
+// Alternatively
+var lengthOfLongestSubstring = function(s) {
+  let seenChars = {};
+    let maxLen = 0;
+    let start = 0;
+    for (let end = 0; end < s.length; end++) {
+      let lastChar = s[end];
+      if (lastChar in seenChars) {
+        start = Math.max(start, seenChars[lastChar] + 1);
+      }
+      seenChars[lastChar] = end;
+      maxLen = Math.max(maxLen, end - start + 1);
+    }
+    return maxLen;
+  };
+  
 console.log(longestSubString("hehloa"))
