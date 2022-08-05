e881b30b38687e1f6cfe2f1a55a88e9c56389265
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-08-04 23:45:23 -0400

    convert a given integer to a roman integer

---
+++ leetCode/integerToRoman.js
@@ -0,0 +1,21 @@
+const intToRoman = function(num) {
+  const symbols = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+    IV: 4,
+    IX: 9,
+    XL: 40,
+    XC: 90,
+    CD: 400,
+    CM: 900,
+  };
+
+  for (let i of num) {
+    return num;
+  }
+};
