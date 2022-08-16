54c7a1d7e51bcef7b81e955a97bebae1d3518302
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-08-15 23:42:12 -0400

    implement permutation function

--- leetCode/permutations.js
+++ leetCode/permutations.js
@@ -1,6 +1,25 @@
 // 46. Permutations
 const permute = function (nums) {
+  // a global result
   const result = [];
 
+  // dfs recursive helper
+  const dsf = function (i, nums) {
+    // base case
+    if (i === nums.length) {
+      result.push(nums.slice());
+      return;
+    }
+    // dfs recursive case
+    for (let j = i; j < nums.length; j++) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      dsf(i + 1, nums);
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+  };
+  dsf(0, nums);
   return result;
 };
+
+// Driver code.
+console.log(permute([1, 2, 3]));
