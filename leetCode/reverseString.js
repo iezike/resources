598915060b8f7e2cbdfe3da6c866ddef34ff6f11
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-08-16 23:06:44 -0400

    reverse string alternative

--- leetCode/reverseString.js
+++ leetCode/reverseString.js
@@ -1,5 +1,13 @@
 // 344. Reverse String
 
-const reverseString = function(s) {
-    return s.reverse();
+const reverseString = function (s) {
+  let left = 0,
+    right = s.length - 1;
+  while (left <= right) {
+    [s[left], s[right]] = [s[right], s[left]];
+    left++;
+    right--;
+  }
+  return s;
 };
+console.log(reverseString(["h", "e", "l", "l", "o"]));
