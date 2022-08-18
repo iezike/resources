7354cce319557b55c3d4a53a562bbe5b9a21f21c
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-08-18 15:06:29 -0400

    listing in python/usr/local/bin/python3 /Users/ikennaezike/lighthouse/resources/python/list.py

---
+++ python/list.py
@@ -0,0 +1,8 @@
+
+list1 = [2, 9, 10, 0, 1]
+list2 = [ "John", "Kim", "Adam"]
+list3 = [30, 40]
+list1.extend(list3)
+list1.extend([3])
+print(list1)
+
--- python/variable.py
+++ python/variable.py
@@ -1,4 +1,14 @@
+from cmath import phase
+
+
 num1 = 1
 num2 = 20
 num3 = num1 + num2
 print(num3)
+
+phrase = "Jonny Kenndy"
+phrase_lower = phrase.lower()
+print(phrase_lower.islower())
+print(len(phrase))
+
+print(phrase.replace("Jonny", "Timothy"))
